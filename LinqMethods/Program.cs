499c652b3e3c3341a3e612c279e2244d2f3199eb
Felipemart123 <felipemart1876@gmail.com>
2022-07-21 10:39:41 -0400

    linq methods

---
+++ LinqMethods/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+List<string> videoGameNames = new() { "Call of Duty", "Fifa", "Super Mario", "Kotor" };
+
+var sortedValues = videoGameNames
+    .OrderByDescending(x => x.Length);
+
+foreach (var name in sortedValues)
+{
+    Console.WriteLine(name);
+}
+
+
